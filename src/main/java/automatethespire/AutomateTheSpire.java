cda9987607f169bf1933f6bc9259d36f267f4712
Dongwon9 <goodongwon329@gmail.com>
2023-12-03 10:54:27 +0900

    이벤트 클릭 지연시킴

--- src/main/java/automatethespire/AutomateTheSpire.java
+++ src/main/java/automatethespire/AutomateTheSpire.java
@@ -19,10 +19,10 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.Hitbox;
-import com.megacrit.cardcrawl.localization.*;
 import com.megacrit.cardcrawl.map.MapRoomNode;
 import com.megacrit.cardcrawl.neow.NeowRoom;
 import com.megacrit.cardcrawl.rewards.RewardItem;
+import com.megacrit.cardcrawl.rewards.chests.AbstractChest;
 import com.megacrit.cardcrawl.rooms.*;
 import com.megacrit.cardcrawl.ui.buttons.LargeDialogOptionButton;
 import com.megacrit.cardcrawl.ui.buttons.ProceedButton;
@@ -35,8 +35,9 @@ import java.util.*;
 @SpireInitializer
 public class AutomateTheSpire implements PostUpdateSubscriber, OnPlayerTurnStartPostDrawSubscriber,
                                          PostBattleSubscriber, PostDeathSubscriber {
-    public static final float cooldown = 0.2f;
+    public static final float cooldown = 0f;
     private static final String resourcesFolder = "automatethespire";
+    private static final float eventButtonDelay = 0.1f;
     public static ModInfo info;
     public static String modID; //Edit your pom.xml to change this
     public static final Logger logger = LogManager.getLogger(modID); //Used to output to the console.
@@ -45,40 +46,44 @@ public class AutomateTheSpire implements PostUpdateSubscriber, OnPlayerTurnStart
         loadModInfo();
     }
 
+    LargeDialogOptionButton prevButton;
     private AbstractRoom currRoom;
-
     private float cooldownLeft = 0f;
     private boolean turnFullyBegun = false;
+    private boolean bossChestOpened = false;
+    /*----------Localization----------*/
+    private boolean mapNodePressed = false;
+    private float eventButtonDelayLeft = 0.1f;
+    //This will be called by ModTheSpire because of the @SpireInitializer annotation at the top of the class.
+    private AbstractRoom.RoomPhase prevPhase;
+    //This is used to load the appropriate localization files based on language.
+    private Class<? extends AbstractRoom> prevRoom;
+    //These methods are used to generate the correct filepaths to various parts of the resources folder.
+    private AbstractDungeon.CurrentScreen prevScreen;
+    //This determines the mod's ID based on information stored by ModTheSpire.
+    private GameActionManager.Phase prevActionPhase;
 
     public AutomateTheSpire() {
         BaseMod.subscribe(this); //This will make BaseMod trigger all the subscribers at their appropriate times.
         logger.info(modID + " subscribed to BaseMod.");
     }
 
-    /*----------Localization----------*/
-
-    //This is used to prefix the IDs of various objects like cards and relics,
-    //to avoid conflicts between different mods using the same name for things.
     public static String makeID(String id) {
         return modID + ":" + id;
     }
 
-    //This will be called by ModTheSpire because of the @SpireInitializer annotation at the top of the class.
     public static void initialize() {
         new AutomateTheSpire();
     }
 
-    //This is used to load the appropriate localization files based on language.
     private static String getLangString() {
         return Settings.language.name().toLowerCase();
     }
 
-    //These methods are used to generate the correct filepaths to various parts of the resources folder.
     public static String localizationPath(String lang, String file) {
         return resourcesFolder + "/localization/" + lang + "/" + file;
     }
 
-    //This determines the mod's ID based on information stored by ModTheSpire.
     private static void loadModInfo() {
         Optional<ModInfo> infos = Arrays.stream(Loader.MODINFOS).filter((modInfo) -> {
             AnnotationDB annotationDB = Patcher.annotationDBMap.get(modInfo.jarURL);
@@ -130,10 +135,8 @@ public class AutomateTheSpire implements PostUpdateSubscriber, OnPlayerTurnStart
             return;
         }
         for (RewardItem reward : AbstractDungeon.combatRewardScreen.rewards) {
-            if (reward.type != RewardItem.RewardType.CARD && !(reward.type == RewardItem.RewardType.RELIC &&
-                                                               AbstractDungeon.getCurrRoom().getClass() ==
-                                                               TreasureRoom.class && Settings.isFinalActAvailable &&
-                                                               !Settings.hasSapphireKey) &&
+            if (reward.type != RewardItem.RewardType.CARD &&
+                !(reward.type == RewardItem.RewardType.RELIC && reward.relicLink != null) &&
                 reward.type != RewardItem.RewardType.SAPPHIRE_KEY) {
                 logger.info("Taking Reward : " + reward.type);
                 reward.isDone = true;
@@ -145,94 +148,141 @@ public class AutomateTheSpire implements PostUpdateSubscriber, OnPlayerTurnStart
         }
     }
 
-    private static void PressMapNode() {
-        if (AbstractDungeon.screen == AbstractDungeon.CurrentScreen.MAP) {
-            if (AbstractDungeon.currMapNode.y == 14 ||
-                (AbstractDungeon.id.equals("TheEnding") && AbstractDungeon.currMapNode.y == 2)) {
-                DungeonMapPatch.doBossHover = true;
-            }
-            ArrayList<MapRoomNode> choices = getMapScreenNodeChoices();
-            if (choices.size() == 1) {
-                MapRoomNodeHoverPatch.hoverNode = choices.get(0);
-                MapRoomNodeHoverPatch.doHover = true;
-                AbstractDungeon.dungeonMapScreen.clicked = true;
-
-            }
-        }
-    }
-
-    private static void PressEventButton() {
-        if ((AbstractDungeon.getCurrRoom() instanceof EventRoom || AbstractDungeon.getCurrRoom() instanceof NeowRoom ||
-             AbstractDungeon.getCurrRoom() instanceof VictoryRoom) &&
-            AbstractDungeon.screen == AbstractDungeon.CurrentScreen.NONE) {
-            ArrayList<LargeDialogOptionButton> activeButtons = EventScreenUtils.getActiveEventButtons();
-            if (activeButtons.size() == 1) {
-                activeButtons.get(0).pressed = true;
-            }
-        }
-    }
-
-
-
-    private void loadLocalization(String lang) {
-        //While this does load every type of localization, most of these files are just outlines so that you can see
-        // how they're formatted.
-        //Feel free to comment out/delete any that you don't end up using.
-        BaseMod.loadCustomStringsFile(CardStrings.class, localizationPath(lang, "CardStrings.json"));
-        BaseMod.loadCustomStringsFile(CharacterStrings.class, localizationPath(lang, "CharacterStrings.json"));
-        BaseMod.loadCustomStringsFile(EventStrings.class, localizationPath(lang, "EventStrings.json"));
-        BaseMod.loadCustomStringsFile(OrbStrings.class, localizationPath(lang, "OrbStrings.json"));
-        BaseMod.loadCustomStringsFile(PotionStrings.class, localizationPath(lang, "PotionStrings.json"));
-        BaseMod.loadCustomStringsFile(PowerStrings.class, localizationPath(lang, "PowerStrings.json"));
-        BaseMod.loadCustomStringsFile(RelicStrings.class, localizationPath(lang, "RelicStrings.json"));
-        BaseMod.loadCustomStringsFile(UIStrings.class, localizationPath(lang, "UIStrings.json"));
-    }
-
+    //This is used to prefix the IDs of various objects like cards and relics,
+    //to avoid conflicts between different mods using the same name for things.
     @Override
     public void receivePostUpdate() {
         cooldownLeft -= Gdx.graphics.getDeltaTime();
         if (cooldownLeft > 0 || !CardCrawlGame.isInARun() || !AbstractDungeon.isPlayerInDungeon()) {
             return;
-        }try{
+        }
+        try {
             currRoom = AbstractDungeon.getCurrRoom();
-        }catch (Exception e){
+        } catch (Exception e) {
             return;
         }
+        //DebugRoomAndPhaseInfo();
         cooldownLeft = cooldown;
         PressEventButton();
         PressEndTurn();
         PressMapNode();
         TakeCombatReward();
         PressProceed();
+        OpenChest();
+    }
+
+    private void DebugRoomAndPhaseInfo() {
+        if (prevPhase != currRoom.phase) {
+            logger.info("Phase: " + currRoom.phase);
+            prevPhase = currRoom.phase;
+        }
+        if (prevRoom != currRoom.getClass()) {
+            logger.info("Room: " + currRoom.getClass());
+            prevRoom = currRoom.getClass();
+        }
+        if (prevScreen != AbstractDungeon.screen) {
+            logger.info("Screen " + AbstractDungeon.screen);
+            prevScreen = AbstractDungeon.screen;
+        }
+        if (prevActionPhase != AbstractDungeon.actionManager.phase) {
+            logger.info("ActionPhase : " + AbstractDungeon.actionManager.phase);
+            prevActionPhase = AbstractDungeon.actionManager.phase;
+        }
+    }
+
+    private void PressEventButton() {
+        if (!(AbstractDungeon.getCurrRoom() instanceof EventRoom) &&
+             !(AbstractDungeon.getCurrRoom() instanceof NeowRoom) &&
+             !(AbstractDungeon.getCurrRoom() instanceof VictoryRoom)){
+            return;
+        }
+        if (AbstractDungeon.screen != AbstractDungeon.CurrentScreen.NONE) {
+            eventButtonDelayLeft = eventButtonDelay * 24;
+            return;
+        }
+        ArrayList<LargeDialogOptionButton> activeButtons = EventScreenUtils.getActiveEventButtons();
+        if (activeButtons.size() == 1) {
+            eventButtonDelayLeft -= Gdx.graphics.getRawDeltaTime() * 6;
+            if (eventButtonDelayLeft <= 0 && activeButtons.get(0) != prevButton) {
+                activeButtons.get(0).pressed = true;
+                prevButton = activeButtons.get(0);
+                eventButtonDelayLeft = 0;
+                logger.info("Pressed Event Option");
+
+            }
+        }
+
+    }
+
+    private void PressMapNode() {
+        if (AbstractDungeon.screen != AbstractDungeon.CurrentScreen.MAP) {
+            mapNodePressed = false;
+            return;
+        }
+        if (mapNodePressed) {
+            return;
+        }
+        mapNodePressed = true;
+        if (AbstractDungeon.currMapNode.y == 14 ||
+            (AbstractDungeon.id.equals("TheEnding") && AbstractDungeon.currMapNode.y == 2)) {
+            DungeonMapPatch.doBossHover = true;
+            return;
+        }
+        logger.info("click!");
+        AbstractDungeon.dungeonMapScreen.clicked = true;
+        ArrayList<MapRoomNode> choices = getMapScreenNodeChoices();
+        if (choices.size() == 1) {
+            MapRoomNodeHoverPatch.hoverNode = choices.get(0);
+            MapRoomNodeHoverPatch.doHover = true;
+        }
+    }
+
+    private void OpenChest() {
+        if (!(currRoom instanceof TreasureRoomBoss)) {
+            bossChestOpened = false;
+        }
+        AbstractChest chest = null;
+        if (currRoom instanceof TreasureRoomBoss && !bossChestOpened) {
+            chest = ((TreasureRoomBoss) AbstractDungeon.getCurrRoom()).chest;
+            bossChestOpened = true;
+        }
+        if (currRoom instanceof TreasureRoom && !AbstractDungeon.player.hasRelic("Cursed Key")) {
+            chest = ((TreasureRoom) AbstractDungeon.getCurrRoom()).chest;
+        }
+        if (chest != null && !chest.isOpen) {
+            chest.isOpen = true;
+            chest.open(false);
+        }
     }
 
     private void PressProceed() {
-        if (currRoom instanceof TreasureRoomBoss ||
-            currRoom instanceof RestRoom ||
-            (AbstractDungeon.id.equals("TheEnding") || AbstractDungeon.id.equals("TheBeyond")) &&
-            currRoom instanceof MonsterRoomBoss) {
-            if (currRoom instanceof TreasureRoomBoss &&
-                !((TreasureRoomBoss) currRoom).chest.isOpen) {
-                return;
-            }
-            if (currRoom instanceof RestRoom && !CampfireUI.hidden) {
-                return;
-            }
-            if (!(boolean) ReflectionHacks.getPrivate(AbstractDungeon.overlayMenu.proceedButton, ProceedButton.class,
-                    "isHidden")) {
-                Hitbox hb = ReflectionHacks.getPrivate(AbstractDungeon.overlayMenu.proceedButton, ProceedButton.class
-                        , "hb");
-                hb.clicked = true;
-            }
+        if (//!(currRoom instanceof TreasureRoomBoss) &&
+                !(currRoom instanceof RestRoom) &&
+                ((!AbstractDungeon.id.equals("TheEnding") && !AbstractDungeon.id.equals("TheBeyond")) ||
+                 !(currRoom instanceof MonsterRoomBoss))) {
+            return;
+        }
+//        if (currRoom instanceof TreasureRoomBoss &&
+//            !((TreasureRoomBoss) currRoom).chest.isOpen) {
+//            return;
+//        }
+        if (currRoom instanceof RestRoom && !CampfireUI.hidden) {
+            return;
+        }
+        if (!(boolean) ReflectionHacks.getPrivate(AbstractDungeon.overlayMenu.proceedButton, ProceedButton.class,
+                "isHidden")) {
+            Hitbox hb = ReflectionHacks.getPrivate(AbstractDungeon.overlayMenu.proceedButton, ProceedButton.class,
+                    "hb");
+            hb.clicked = true;
         }
     }
 
 
     private void PressEndTurn() {
         if (turnFullyBegun && AbstractDungeon.actionManager.phase == GameActionManager.Phase.WAITING_ON_USER &&
-            currRoom.phase == AbstractRoom.RoomPhase.COMBAT &&
-            !AbstractDungeon.actionManager.turnHasEnded && AbstractDungeon.actionManager.actions.isEmpty() &&
-            !AbstractDungeon.player.hand.canUseAnyCard() && !AbstractDungeon.player.hasAnyPotions()) {
+            currRoom.phase == AbstractRoom.RoomPhase.COMBAT && !AbstractDungeon.actionManager.turnHasEnded &&
+            AbstractDungeon.actionManager.actions.isEmpty() && !AbstractDungeon.player.hand.canUseAnyCard() &&
+            !AbstractDungeon.player.hasAnyPotions()) {
             AbstractDungeon.actionManager.addToBottom(new PressEndTurnButtonAction());
             logger.info("Ended turn!");
             turnFullyBegun = false;
